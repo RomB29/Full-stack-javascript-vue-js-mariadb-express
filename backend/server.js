37892b214c4fa00d7a0070925ec9a1772ea216f2
Romain <81963300+RomB29@users.noreply.github.com>
2021-08-20 00:36:18 +0200

    front + backend

--- backend/server.js
+++ backend/server.js
@@ -2,32 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const path = __dirname + '/app/views/';
+
 const app = express();
-const db  = require("./app/models");
-db.sequelize.sync({
-    force: true}).then(() => {
-    // In development, you may need to drop existing tables and re-sync database. Just use force: true as following code:
-    console.log("Drop and re-sync db");
-});
+
+app.use(express.static(path));
+
 var corsOptions = {
-    origin: "http://localhost:8081"
+  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to RomB29 application." });
+const db = require("./app/models");
+
+db.sequelize.sync();
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
 });
+
+require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
